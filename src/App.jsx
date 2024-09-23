5d90ed092dfce9184dd692558a4e6b9613ada486
occemper <ravilre22@gmail.com>
2024-09-23 23:49:20 +0300

    020

--- src/App.jsx
+++ src/App.jsx
@@ -6,47 +6,31 @@ import './App.css'
 
 class App extends Component {
   state = {
-    count: { first: 0, second: 23 },
-    pount: 12
+    monsters: [
+      {
+        name: 'Yago'
+      },
+      {
+        name: 'Bortolomeo'
+      },
+      {
+        name: 'Frank'
+      },
+      {
+        name: 'Linda'
+      },
+    ]
+
   }
   render() {
     return (
-      <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button
-            onClick={() => {
-              this.setState((state, props) => {
-                return {
-                  count: {
-                    first: state.count.first + 12
-                    , second: state.count.second + 23
-                  }
-                };
-              },
-                () => {
-                  console.log(this.state);
-                });
-            }}
-          >
-            count is {this.state.count.first}; {this.state.count.second}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR{this.state.pount}
-          </p>
-        </div >
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
+      <div className='App'>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
+      </div>
     );
   }
 }
