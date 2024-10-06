7fadfa3af9c9fd0702c9b79a10ade252e8b18dfb
occemper <ravilre22@gmail.com>
2024-10-07 01:01:44 +0300

    043

--- src/App.jsx
+++ src/App.jsx
@@ -5,59 +5,74 @@ import './App.css'
 import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component'
 
-class App extends Component {
-  state = {
-    robots: [],
-    searchField: ''
-  };
+const App = () => {
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json()
-        .then((users) => this.setState(
-          () => {
-            return {
-              robots: users,
-            };
-          }
-        ))
-      );
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Robots Rolodex</h1>
 
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleUpperCase();
-    this.setState(
-      () => {
-        return { searchField };
-      });
-  }
-
-  render() {
-
-
-    const { robots: robots, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredRobots = robots
-      .filter(x =>
-        x.name
-          .toLocaleUpperCase()
-          .includes(searchField)
-      );
-
-    return (
-      <div className='App'>
-        <h1 className='app-title'>Robots Rolodex</h1>
-
-        <SearchBox
-          className={'robots-search-box'}
-          onChangeHandler={onSearchChange}
-          placeholder={'search robots'} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
-  }
+      {/* <SearchBox
+        className={'robots-search-box'}
+        onChangeHandler={onSearchChange}
+        placeholder={'search robots'} />
+      <CardList robots={filteredRobots} /> */}
+    </div>
+  )
 }
+
+// class App extends Component {
+//   state = {
+//     robots: [],
+//     searchField: ''
+//   };
+
+//   componentDidMount() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json()
+//         .then((users) => this.setState(
+//           () => {
+//             return {
+//               robots: users,
+//             };
+//           }
+//         ))
+//       );
+
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleUpperCase();
+//     this.setState(
+//       () => {
+//         return { searchField };
+//       });
+//   }
+
+//   render() {
+
+
+//     const { robots: robots, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredRobots = robots
+//       .filter(x =>
+//         x.name
+//           .toLocaleUpperCase()
+//           .includes(searchField)
+//       );
+
+//     return (
+//       <div className='App'>
+//         <h1 className='app-title'>Robots Rolodex</h1>
+
+//         <SearchBox
+//           className={'robots-search-box'}
+//           onChangeHandler={onSearchChange}
+//           placeholder={'search robots'} />
+//         <CardList robots={filteredRobots} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App
